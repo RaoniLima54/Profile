bee3c8ea447b62e716182bbdedfee9d031257e27
Raoni Lima <raonesantoslima54@gmail.com>
2025-05-25 14:35:32 -0300

    Responsivity

--- assets/scripty.js
+++ assets/scripty.js
@@ -1,17 +1,10 @@
-const slides = document.querySelector('.slides');
-  const totalSlides = document.querySelectorAll('.slides img').length;
-  let index = 0;
+let index = 0;
+  const track = document.getElementById('track');
+  const slides = document.querySelectorAll('.carousel-slide');
 
-  document.getElementById('next').addEventListener('click', () => {
-    index = (index + 1) % totalSlides;
-    updateCarousel();
-  });
-
-  document.getElementById('prev').addEventListener('click', () => {
-    index = (index - 1 + totalSlides) % totalSlides;
-    updateCarousel();
-  });
-
-  function updateCarousel() {
-    slides.style.transform = 'translateX(' + (-index * 100) + '%)';
+  function moveSlide(direction) {
+    index += direction;
+    if (index < 0) index = slides.length - 1;
+    if (index >= slides.length) index = 0;
+    track.style.transform = `translateX(-${index * 100}%)`;
   }
